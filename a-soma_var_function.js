c95258c01b11c67803fb6cec680444b3e64eeeef
nicolas71488 <159440979+nicolas71488@users.noreply.github.com>
2024-04-11 09:04:06 -0300

    Add files via upload

---
+++ a-soma_var_function.js
@@ -0,0 +1,12 @@
+const num1 = 25;
+const num2 = 25;
+const resultado = num1 + num2;
+
+///mesma função de codigo - fazem a mesma caisa
+
+function soma(num1, num2)
+{
+    return num1 + num2;
+}
+    console.log(soma(5, 5))
+    console.log(resultado)
---
+++ d-notas_puxe.js
@@ -0,0 +1,11 @@
+//ARRAY
+const notas = [10, 6, 8]
+notas.push(7);
+console.log(notas);
+const media = (notas[0] + notas[1] + notas[2]) / notas.length;
+console.log(media);
+
+//const notas = [10, 6, 8]
+//notas.push(7);
+//const media = (notas[0] + notas[1] + notas[2]) / notas.length;
+//console.log(media);
